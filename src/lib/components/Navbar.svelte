37d3f31667e4a72407ab994745d007895b9af909
Hayden Hanson <hhanson2@gmu.edu>
2024-06-13 10:02:10 -0400

    fix: update padding for nav on mobile

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,86 +1,86 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import ThemeSwitcher from './ThemeSwitcher.svelte';
+  import { page } from '$app/stores';
+  import ThemeSwitcher from './ThemeSwitcher.svelte';
 
-	let previousY: number;
-	let currentY: number;
-	let clientHeight: number;
+  let previousY: number;
+  let currentY: number;
+  let clientHeight: number;
 
-	// decides whether client is scrolling down or up
-	const deriveDirection = (y: number) => {
-		const direction = !previousY || previousY < y ? 'down' : 'up';
-		previousY = y;
+  // decides whether client is scrolling down or up
+  const deriveDirection = (y: number) => {
+    const direction = !previousY || previousY < y ? 'down' : 'up';
+    previousY = y;
 
-		return direction;
-	};
+    return direction;
+  };
 
-	$: scrollDirection = deriveDirection(currentY);
-	$: offscreen = scrollDirection === 'down' && currentY > clientHeight * 8;
+  $: scrollDirection = deriveDirection(currentY);
+  $: offscreen = scrollDirection === 'down' && currentY > clientHeight * 8;
 
-	$: url = $page.url.href;
-	$: routeId = $page.url.pathname;
+  $: url = $page.url.href;
+  $: routeId = $page.url.pathname;
 
-	const links = [
-		{
-			title: 'Blog',
-			href: '/'
-		},
-		{
-			title: 'Projects',
-			href: '/projects'
-		},
-		{
-			title: 'About',
-			href: '/about'
-		},
-		{
-			title: 'Contact',
-			href: '/contact'
-		}
-	];
+  const links = [
+    {
+      title: 'Blog',
+      href: '/'
+    },
+    {
+      title: 'Projects',
+      href: '/projects'
+    },
+    {
+      title: 'About',
+      href: '/about'
+    },
+    {
+      title: 'Contact',
+      href: '/contact'
+    }
+  ];
 </script>
 
 <svelte:window bind:scrollY={currentY} />
 
 <nav
-	class="fixed inset-x-0 z-50 mx-auto mt-6 flex h-16 w-fit items-center justify-between
+  class="fixed inset-x-0 z-50 mx-auto mt-6 flex h-16 w-fit items-center justify-between
 	rounded-3xl border border-crust bg-background backdrop-blur-3xl dark:bg-transparent
-	transition-transform ease-in sm:justify-center gap-4 sm:gap-8 px-6 sm:px-12"
-	class:motion-safe:-translate-y-24={offscreen}
-	bind:clientHeight
+	transition-transform ease-in sm:justify-center gap-4 sm:gap-8 px-4 sm:px-12"
+  class:motion-safe:-translate-y-24={offscreen}
+  bind:clientHeight
 >
-	{#each links as { title, href }}
-		{#if title === 'Blog'}
-			<a
-				{href}
-				{title}
-				class:active={href === '/' ? routeId === '/' : url.includes(href)}
-				class="flex flex-row items-center justify-center gap-2 text-sm sm:text-lg"
-			>
-				{title}
-				<span
-					class="hidden h-5 w-5 border-crust font-normal rounded-md border text-center text-sm sm:block"
-					>{`/`}</span
-				>
-			</a>
-		{:else}
-			<a
-				{href}
-				{title}
-				class:active={href === '/' ? routeId === '/' : url.includes(href)}
-				class="text-sm sm:text-lg"
-			>
-				{title}
-			</a>
-		{/if}
-	{/each}
+  {#each links as { title, href }}
+    {#if title === 'Blog'}
+      <a
+        {href}
+        {title}
+        class:active={href === '/' ? routeId === '/' : url.includes(href)}
+        class="flex flex-row items-center justify-center gap-2 text-sm sm:text-lg"
+      >
+        {title}
+        <span
+          class="hidden h-5 w-5 border-crust font-normal rounded-md border text-center text-sm sm:block"
+          >{`/`}</span
+        >
+      </a>
+    {:else}
+      <a
+        {href}
+        {title}
+        class:active={href === '/' ? routeId === '/' : url.includes(href)}
+        class="text-sm sm:text-lg"
+      >
+        {title}
+      </a>
+    {/if}
+  {/each}
 
-	<ThemeSwitcher />
+  <ThemeSwitcher />
 </nav>
 
 <style lang="postcss">
-	.active {
-		color: theme(colors.primary);
-		font-weight: 525;
-	}
+  .active {
+    color: theme(colors.primary);
+    font-weight: 525;
+  }
 </style>
