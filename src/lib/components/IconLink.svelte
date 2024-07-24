9ab90b6cd693103a6746ab4c3f3835c8c5759815
Hayden <haydenhansondev+GitHub@gmail.com>
2024-07-24 12:54:36 -0400

    fix: icon link looks more like a button now

--- src/lib/components/IconLink.svelte
+++ src/lib/components/IconLink.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	export let href: string;
-	export let title: string;
-	export let target: '_self' | '_blank' = '_self';
+  export let href: string;
+  export let title: string;
+  export let target: '_self' | '_blank' = '_self';
 </script>
 
 <a
-	class="rounded-lg p-2 hover:bg-crust flex flex-row gap-2 items-center sm:gap-4 z-30"
-	{href}
-	{target}
-	{title}
+  class="rounded-lg p-2 shadow-md hover:bg-crust flex flex-row gap-2 items-center sm:gap-4 z-30"
+  {href}
+  {target}
+  {title}
 >
-	<slot />
+  <slot />
 </a>
