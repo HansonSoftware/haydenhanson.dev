a64005aea80e7c7cb3a990ecc5b9bb81bec9036b
Hayden <haydenhansondev+GitHub@gmail.com>
2024-07-26 10:33:50 -0400

    change: asc order for projects

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -1,44 +1,47 @@
-import type { MarkdownProject, ProjectMetadataAndSlug } from '../../../types/index.ts';
+import type {
+  MarkdownProject,
+  ProjectMetadataAndSlug
+} from '../../../types/index.ts';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
-	// use vite glob import to get all project write-ups
-	const markdownProjectModules = import.meta.glob('/src/projects/*') as Record<
-		string,
-		() => Promise<MarkdownProject>
-	>;
+  // use vite glob import to get all project write-ups
+  const markdownProjectModules = import.meta.glob('/src/projects/*') as Record<
+    string,
+    () => Promise<MarkdownProject>
+  >;
 
-	const projectPromises: Promise<ProjectMetadataAndSlug>[] = [];
+  const projectPromises: Promise<ProjectMetadataAndSlug>[] = [];
 
-	for (const path in markdownProjectModules) {
-		const loadMarkdownProjectModule = markdownProjectModules[path];
+  for (const path in markdownProjectModules) {
+    const loadMarkdownProjectModule = markdownProjectModules[path];
 
-		const loadProjectSlugAndMetadata = async function () {
-			// dynamically import markdown project
-			const markdownProjectModule = await loadMarkdownProjectModule();
+    const loadProjectSlugAndMetadata = async function () {
+      // dynamically import markdown project
+      const markdownProjectModule = await loadMarkdownProjectModule();
 
-			// slug is everything after last / without the file extension
-			const slug = path.slice(path.lastIndexOf('/') + 1).replace('.md', '');
+      // slug is everything after last / without the file extension
+      const slug = path.slice(path.lastIndexOf('/') + 1).replace('.md', '');
 
-			return {
-				slug,
-				metadata: markdownProjectModule.metadata
-			};
-		};
+      return {
+        slug,
+        metadata: markdownProjectModule.metadata
+      };
+    };
 
-		projectPromises.push(loadProjectSlugAndMetadata());
-	}
+    projectPromises.push(loadProjectSlugAndMetadata());
+  }
 
-	// load all projects concurrently
-	const projects = await Promise.all(projectPromises);
+  // load all projects concurrently
+  const projects = await Promise.all(projectPromises);
 
-	// sort by publication date (descending/most recent first)
-	const sortedProjects = projects.sort((project1, project2) => {
-		return (
-			new Date(project2.metadata.completedAt).getTime() -
-			new Date(project1.metadata.completedAt).getTime()
-		);
-	});
+  // sort by project date (ascending/oldest first)
+  const sortedProjects = projects.sort((project2, project1) => {
+    return (
+      new Date(project2.metadata.completedAt).getTime() -
+      new Date(project1.metadata.completedAt).getTime()
+    );
+  });
 
-	return json(sortedProjects);
+  return json(sortedProjects);
 };
