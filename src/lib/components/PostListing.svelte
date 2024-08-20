b6ff82b12160c99661086bfd107a4db4a7d96d49
Hayden <haydenhansondev+GitHub@gmail.com>
2024-08-20 08:51:45 -0400

    fix: reduce img size in blog post preview

--- src/lib/components/PostListing.svelte
+++ src/lib/components/PostListing.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <article class="p-4 sm:flex sm:space-x-4">
-  <a {href} class="block sm:w-80">
+  <a {href} class="block sm:w-72">
     <div
       class="aspect-h-9 aspect-w-16 overflow-hidden rounded-md border border-foreground"
     >
