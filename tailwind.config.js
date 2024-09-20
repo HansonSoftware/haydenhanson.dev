c24b58a27bee05b0f4dcfc7c452f3c73cdca21ad
Hayden Hanson <hansonhayd3n@gmail.com>
2024-09-19 22:32:20 -0400

    fix: remove backtick rendering!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			fontFamily: {
-				montserrat: ['Montserrat']
-			},
-			colors: {
-				background: 'var(--background)',
-				foreground: 'var(--foreground)',
-				card: 'var(--card)',
-				'card-foreground': 'var(--card-foreground)',
-				popover: 'var(--popover)',
-				'popover-foreground': 'var(--popover-foreground)',
-				primary: 'var(--primary)',
-				'primary-foreground': 'var(--primary-foreground)',
-				secondary: 'var(--secondary)',
-				'secondary-foreground': 'var(--secondary-foreground)',
-				muted: 'var(--muted)',
-				'muted-foreground': 'var(--muted-foreground)',
-				accent: 'var(--accent)',
-				'accent-foreground': 'var(--accent-foreground)',
-				destructive: 'var(--destructive)',
-				'destructive-foreground': 'var(--destructive-foreground)',
-				crust: 'var(--crust)',
-				input: 'var(--input)',
-				ring: 'var(--ring)'
-			},
-			borderRadius: {
-				DEFAULT: 'var(--radius)'
-			}
-		}
-	},
-	variants: {
-		extend: {}
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/aspect-ratio')
-	]
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat']
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        card: 'var(--card)',
+        'card-foreground': 'var(--card-foreground)',
+        popover: 'var(--popover)',
+        'popover-foreground': 'var(--popover-foreground)',
+        primary: 'var(--primary)',
+        'primary-foreground': 'var(--primary-foreground)',
+        secondary: 'var(--secondary)',
+        'secondary-foreground': 'var(--secondary-foreground)',
+        muted: 'var(--muted)',
+        'muted-foreground': 'var(--muted-foreground)',
+        accent: 'var(--accent)',
+        'accent-foreground': 'var(--accent-foreground)',
+        destructive: 'var(--destructive)',
+        'destructive-foreground': 'var(--destructive-foreground)',
+        crust: 'var(--crust)',
+        input: 'var(--input)',
+        ring: 'var(--ring)'
+      },
+      borderRadius: {
+        DEFAULT: 'var(--radius)'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+  ]
 };
